609dae8843e39d7251590dda5fa216c1ac15cbf5
Ecasept <76942454+Ecasept@users.noreply.github.com>
2026-06-05 16:08:11 +0200

    refactor: remove unused source set

--- client/src/prod/java/dev/ecasept/unitodo/client/api/HttpClientFactory.java
+++
@@ -1,9 +0,0 @@
-package dev.ecasept.unitodo.client.api;
-
-import java.net.http.HttpClient;
-
-public class HttpClientFactory {
-    public static HttpClient createClient() {
-        return HttpClient.newHttpClient();
-    }
-}
