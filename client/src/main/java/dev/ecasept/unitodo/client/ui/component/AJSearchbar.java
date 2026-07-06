67de0c9ba2e54e33d9c322dd003cfb9298941af2
Queraxe <84202996+Queraxe@users.noreply.github.com>
2026-07-06 14:34:09 +0200

    chore: javadoc for AJSearchbar ;)

--- client/src/main/java/dev/ecasept/unitodo/client/ui/component/AJSearchbar.java
+++ client/src/main/java/dev/ecasept/unitodo/client/ui/component/AJSearchbar.java
@@ -7,8 +7,16 @@ import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.util.function.Consumer;
 
+/**
+ * A search bar from AJ, you have a text input and the shown tasks are filtered by the input. (name and description of the task)
+ */
 public class AJSearchbar extends JPanel {
     private boolean shouldUpdate = true;
+
+    /**
+     * Creates a new AJSearchbar with a callback that is called when the input changes.
+     * @param callback A callback with String parameter for search input.
+     */
     public AJSearchbar(Consumer<String> callback) {
         this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
 
