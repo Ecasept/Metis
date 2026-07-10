d30f86b193027b02b5cb1b75fe6d49beff5207d0
Ecasept <76942454+Ecasept@users.noreply.github.com>
2026-07-10 13:08:51 +0200

    refactor: remove unused import

--- shared/src/main/java/dev/ecasept/unitodo/shared/db/querybuilder/SortOrder.java
+++ shared/src/main/java/dev/ecasept/unitodo/shared/db/querybuilder/SortOrder.java
@@ -2,7 +2,6 @@ package dev.ecasept.unitodo.shared.db.querybuilder;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
