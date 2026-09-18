618245015e3dff07298b052ad8991f4bb6850d29
Ecasept <76942454+Ecasept@users.noreply.github.com>
2026-09-18 14:23:39 +0200

    build: change to java 25

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,3 @@
 subprojects {
     apply(plugin = "java")
-
-    extensions.configure<JavaPluginExtension> {
-        toolchain {
-            languageVersion = JavaLanguageVersion.of(21)
-        }
-    }
 }
